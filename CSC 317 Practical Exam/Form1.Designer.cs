4b0a8286dccfd9158c61087fafbbd7aa79ccc1b1
Tyler Shoemake <w977584@usm.edu>
2021-11-30 19:12:20 -0600

    created all 6 elements. added images to resources for picture updates

--- CSC 317 Practical Exam/Form1.Designer.cs
+++ CSC 317 Practical Exam/Form1.Designer.cs
@@ -31,7 +31,12 @@ namespace CSC_317_Practical_Exam
         {
             this.numberboxTempValue = new System.Windows.Forms.NumericUpDown();
             this.comboUnits = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblConversion1Value = new System.Windows.Forms.Label();
+            this.lblConversion2Value = new System.Windows.Forms.Label();
+            this.imageTempGuage = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.numberboxTempValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTempGuage)).BeginInit();
             this.SuspendLayout();
             // 
             // numberboxTempValue
@@ -64,11 +69,56 @@ namespace CSC_317_Practical_Exam
             this.comboUnits.Size = new System.Drawing.Size(121, 26);
             this.comboUnits.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Starting Temperature:";
+            // 
+            // lblConversion1Value
+            // 
+            this.lblConversion1Value.AutoSize = true;
+            this.lblConversion1Value.BackColor = System.Drawing.Color.White;
+            this.lblConversion1Value.Location = new System.Drawing.Point(318, 191);
+            this.lblConversion1Value.Name = "lblConversion1Value";
+            this.lblConversion1Value.Size = new System.Drawing.Size(68, 18);
+            this.lblConversion1Value.TabIndex = 3;
+            this.lblConversion1Value.Text = "value1";
+            this.lblConversion1Value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblConversion2Value
+            // 
+            this.lblConversion2Value.AutoSize = true;
+            this.lblConversion2Value.BackColor = System.Drawing.Color.White;
+            this.lblConversion2Value.Location = new System.Drawing.Point(318, 229);
+            this.lblConversion2Value.Name = "lblConversion2Value";
+            this.lblConversion2Value.Size = new System.Drawing.Size(68, 18);
+            this.lblConversion2Value.TabIndex = 4;
+            this.lblConversion2Value.Text = "value2";
+            this.lblConversion2Value.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // imageTempGuage
+            // 
+            this.imageTempGuage.Cursor = System.Windows.Forms.Cursors.Default;
+            this.imageTempGuage.Location = new System.Drawing.Point(81, 154);
+            this.imageTempGuage.Name = "imageTempGuage";
+            this.imageTempGuage.Size = new System.Drawing.Size(204, 93);
+            this.imageTempGuage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageTempGuage.TabIndex = 5;
+            this.imageTempGuage.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1333, 623);
+            this.Controls.Add(this.imageTempGuage);
+            this.Controls.Add(this.lblConversion2Value);
+            this.Controls.Add(this.lblConversion1Value);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboUnits);
             this.Controls.Add(this.numberboxTempValue);
             this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -76,7 +126,9 @@ namespace CSC_317_Practical_Exam
             this.Name = "Form1";
             this.Text = "Temperature Conversion Program";
             ((System.ComponentModel.ISupportInitialize)(this.numberboxTempValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTempGuage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -84,6 +136,10 @@ namespace CSC_317_Practical_Exam
 
         private System.Windows.Forms.NumericUpDown numberboxTempValue;
         private System.Windows.Forms.ComboBox comboUnits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblConversion1Value;
+        private System.Windows.Forms.Label lblConversion2Value;
+        private System.Windows.Forms.PictureBox imageTempGuage;
     }
 }
 
