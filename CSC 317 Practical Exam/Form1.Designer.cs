d8b1fe6c038fd0acf811a448533b4c15786c9429
Tyler Shoemake <w977584@usm.edu>
2021-11-30 18:56:15 -0600

    started project. set font, size, and named form. added numupdown box

--- CSC 317 Practical Exam/Form1.Designer.cs
+++ CSC 317 Practical Exam/Form1.Designer.cs
@@ -29,22 +29,35 @@ namespace CSC_317_Practical_Exam
         /// </summary>
         private void InitializeComponent()
         {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(321, 101);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1333, 623);
+            this.Controls.Add(this.numericUpDown1);
             this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
             this.Text = "Temperature Conversion Program";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
